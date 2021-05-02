d158711dc567e3d967a1043df8df0b999f9ddc99
Mosa Alhaj <mosa.haj2016@outlook.com>
2021-05-02 15:37:41 +0300

    Clean Code

--- app/src/main/java/com/mosaalhaj/zillow/adapter/PostAdapter.java
+++ app/src/main/java/com/mosaalhaj/zillow/adapter/PostAdapter.java
@@ -1,14 +1,11 @@
 package com.mosaalhaj.zillow.adapter;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,23 +24,23 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
     private final Context context;
     private final ArrayList<Post> posts;
-    private final ImageListener imageListener ;
+    private final ImageListener imageListener;
     private final PostListListener listener;
-    private Post post ;
+    private Post post;
 
-    public PostAdapter(Context context, ArrayList<Post> posts,PostListListener listener) {
+    public PostAdapter(Context context, ArrayList<Post> posts, PostListListener listener) {
         this.context = context;
         this.posts = posts;
         this.listener = listener;
         imageListener = (position, imageView) -> {
-            if (post != null && post.getImages() !=null && !post.getImages().isEmpty()){
-                String imageUrl = API_URL+post.getImages().get(position).getUrl();
+            if (post != null && post.getImages() != null && !post.getImages().isEmpty()) {
+                String imageUrl = API_URL + post.getImages().get(position).getUrl();
                 Glide.with(context)
                         .load(imageUrl)
                         .placeholder(R.drawable.post_image)
                         .centerCrop().into(imageView);
             }
-            imageView.setOnClickListener(list->listener.onClick(position));
+            imageView.setOnClickListener(list -> listener.onClick(position));
         };
     }
 
@@ -53,7 +50,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
         View view = LayoutInflater.from(context).inflate(R.layout.home_list_item, null);
 
-        return new PostViewHolder(view,listener);
+        return new PostViewHolder(view, listener);
     }
 
     @Override
@@ -63,7 +60,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
         holder.binding.homeListItemTvName.setText(post.getName());
 
-        String address = post.getAddress().getCountryName()+","+post.getAddress().getCityName();
+        String address = post.getAddress().getCountryName() + "," + post.getAddress().getCityName();
 
         holder.binding.homeListItemTvAddress.setText(address);
         holder.binding.homeListItemCvPostPhotos.setPageCount(post.getImages().size());
@@ -73,7 +70,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
     @Override
     public int getItemCount() {
-        if (posts == null||posts.isEmpty())
+        if (posts == null || posts.isEmpty())
             return 0;
         else
             return posts.size();
@@ -81,8 +78,9 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
     public static class PostViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private PostListListener listener;
         public HomeListItemBinding binding;
+        private PostListListener listener;
+
         public PostViewHolder(@NonNull View itemView, PostListListener listener) {
             super(itemView);
             this.listener = listener;
